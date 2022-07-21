ea9f8dbed04b4386973f6245ed51950b552fb9c5
Federico Brigante <me@fregante.com>
2022-07-21 15:04:22 +0700

    Use microbundle

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 // Prettier supports these languages:
 // https://github.com/prettier/prettier/blob/e46aba0ab279c764dc26e0f41f15c55122440c51/src/language-js/embed.js#L13
 
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
 function concatenateTemplateLiteralTag(
 	strings: TemplateStringsArray,
 	...keys: string[]
@@ -18,40 +14,14 @@ function concatenateTemplateLiteralTag(
 Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
 https://prettier.io/docs/en/options.html#embedded-language-formatting
 */
-export const any = concatenateTemplateLiteralTag;
+const any = concatenateTemplateLiteralTag;
 
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const html = concatenateTemplateLiteralTag;
-
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const css = concatenateTemplateLiteralTag;
-
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const gql = concatenateTemplateLiteralTag;
-
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const graphql = concatenateTemplateLiteralTag;
-
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const md = concatenateTemplateLiteralTag;
-
-/**
-Enable highlighting/prettifying when used as html`<div>` or css`.a {}`
-https://prettier.io/docs/en/options.html#embedded-language-formatting
-*/
-export const markdown = concatenateTemplateLiteralTag;
+export {
+	any,
+	any as html,
+	any as css,
+	any as gql,
+	any as graphql,
+	any as md,
+	any as markdown,
+};
